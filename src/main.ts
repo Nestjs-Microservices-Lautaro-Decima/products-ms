05446eb985c5a8d3b47f60cd7d8af178ad170910
Lautaro Decima <decimalautaro@gmail.com>
2024-06-23 19:10:13 -0300

    feat: setting MessagePattern to TCP

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.PORT,
+      },
+    },
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +23,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(envs.PORT);
-  logger.log(`App runing on port ${envs.PORT}`);
+  await app.listen();
+  logger.log(`Products Microservices runing on port ${envs.PORT}`);
 }
 bootstrap();
