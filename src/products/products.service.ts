9e2a2c203f2e6f001ad65ebddec7b525a6a4ec5d
Lautaro Decima <decimalautaro@gmail.com>
2024-06-23 03:05:02 -0300

    feat: adding find by id product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger, OnModuleInit, Query } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -38,8 +43,15 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id },
+    });
+
+    if (!product)
+      throw new NotFoundException(`Product whith id #${id} not found.`);
+
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
